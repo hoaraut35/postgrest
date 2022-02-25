1ab45e7bf8bbd749e43989dad0d497c5fabbcccf
HOARAU Thomas <perso.thomas.hoarau@gmail.com>
2022-02-25 18:33:08 +0100

    add adapter

--- app/src/main/java/com/hoarauthomas/postgrest/UserAdapter.kt
+++ app/src/main/java/com/hoarauthomas/postgrest/UserAdapter.kt
@@ -1,26 +1,39 @@
 package com.hoarauthomas.postgrest
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.hoarauthomas.postgrest.models.CarsResponseItem
 
-class UserAdapter : RecyclerView.Adapter<UserAdapter.ViewHolder>() {
+class UserAdapter (private val myUsers: List<CarsResponseItem>): RecyclerView.Adapter<UserAdapter.ViewHolder>() {
 
     class ViewHolder(itemView:View): RecyclerView.ViewHolder(itemView) {
         val nameTextView = itemView.findViewById<TextView>(R.id.user_name)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        // Inflate the custom layout
+        val contactView = inflater.inflate(R.layout.item_user, parent, false)
+        // Return a new holder instance
+        return ViewHolder(contactView)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        // Get the data model based on position
+        val contact: CarsResponseItem = myUsers.get(position)
+        // Set item views based on your views and data model
+        val textView = holder.nameTextView
+        textView.text = contact.maker
+
+
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return myUsers.size
     }
 
 }
