20acb232f1bc37485a2016c395b2386d2648569d
HOARAU Thomas <perso.thomas.hoarau@gmail.com>
2022-02-25 13:52:56 +0100

    add api interface and pojo class and Gson lib

--- app/src/main/java/com/hoarauthomas/postgrest/api/Postgrestapi.kt
+++ app/src/main/java/com/hoarauthomas/postgrest/api/Postgrestapi.kt
@@ -1,5 +1,6 @@
 package com.hoarauthomas.postgrest.api
 
+import com.hoarauthomas.postgrest.models.CarsResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -7,5 +8,5 @@ import retrofit2.http.Query
 interface Postgrestapi {
 
     @GET("cars")
-    fun getAllCars(@Query("address") address: String): Call<Cars>
+    fun getAllCars(@Query("address") address: String): Call<CarsResponse>
 }
